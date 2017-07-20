ed0722d1bdccfc9d6b2a6c857d33d9153dee137a
MOZ <MOZ@localhost>
2017-07-20 15:53:49 +0300

    My first commit

---
+++ Curs5Git/src/foo/StudentConsumer.java
@@ -0,0 +1,12 @@
+package foo;
+
+public class StudentConsumer {
+
+	public static void main(String[] args) {
+		Student s1 = new Student();
+		s1.setName("Ion");
+		s1.setAge(24);
+		
+		System.out.println("Hello " + s1.getName());
+	}
+}
